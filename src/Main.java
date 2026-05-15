d653b76c9e8b83a27d2d9db682284db6efeb3dde
Kato89179 <mamekou2020@gmail.com>
2026-05-15 11:12:40 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        String item = "昼食";
+        int price = 800;
+
         System.out.println("家計簿アプリ");
+        System.out.println("項目：" + item);
+        System.out.println("金額：" + price + "円");
     }
 }
